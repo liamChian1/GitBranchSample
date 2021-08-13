1211117650c08e93b63879b4dc69b41759d07d12
LiamChiang <fujisun1382@gmail.com>
2021-08-13 09:36:27 +0800

    push again

--- GitShareApp/src/GitBranchApp/MainApp.java
+++ GitShareApp/src/GitBranchApp/MainApp.java
@@ -3,7 +3,7 @@ package GitBranchApp;
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("1st version");
-		//這是一個新家的數學運算版本
+		//這是一個新加的數學運算版本
 		//再推一次
 		Math cMath = new Math();
 		System.out.println(cMath.add(10, 5));
